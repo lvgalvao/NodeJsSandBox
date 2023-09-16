538acc30422f402a3c21b00aa5b045325fea3c76
Luciano Filho <lvgalvaofilho@gmail.com>
2023-09-16 07:20:13 -0300

    add fastify

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-    response.write('Hello World')
+// const server = createServer((request, response) => {
+//     response.write('Hello World')
 
-    return response.end()
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+
+server.get('/', () => {
+    return 'Hello World'
 })
 
-server.listen(3333)
+server.get('/node', () => {
+    return 'Hello Node'
+})
+
+server.listen({
+    port: 3333,
+})
