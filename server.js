cf29c7b32efe75bca17fb9d54a7797d6c3b43e6f
Luciano Filho <lvgalvaofilho@gmail.com>
2023-09-17 09:04:14 -0300

    fastify application

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
-// import { createServer } from 'node:http'
-
-// const server = createServer((request, response) => {
-//     response.write('Hello World')
-
-//     return response.end()
-// })
-
-// server.listen(3333)
-
 import { fastify } from 'fastify'
+
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
+const database = new DatabaseMemory()
 
-server.get('/', () => {
-    return 'Hello World'
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
 })
 
-server.get('/node', () => {
-    return 'Hello Node'
+server.get('/videos', (request) => {
+    const search = request.query.search
+
+    const videos = database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    const { title, description, duration } = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
