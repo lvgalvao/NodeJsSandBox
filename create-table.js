d633848c579fa08cd9fe60390da2a665e6d039bb
Luciano Filho <lvgalvaofilho@gmail.com>
2023-09-17 13:52:04 -0300

    add delete-table.js

--- create-table.js
+++ create-table.js
@@ -1,7 +1,12 @@
 import { sql } from './db.js'
 
+// sql`DROP TABLE IF EXISTS videos`.then(() => {
+//     console.log('Tabela deletada!')
+// })
+
+
 sql`CREATE TABLE videos (
-    id SERIAL PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     duration INT
